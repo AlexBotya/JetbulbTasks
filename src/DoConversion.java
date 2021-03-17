489d1253c6750d907955870d7ecb74c1b84f9b8d
AlexBotya <alex_balg@bk.ru>
2021-03-17 23:07:17 +0300

    Task Complete 1.0

--- src/DoConversion.java
+++ src/DoConversion.java
@@ -1,8 +1,6 @@
-//import javax.management.InvalidAttributeValueException;
-
 public class DoConversion {
     public static void main(String[] args) {
-        String romanNotation = "LIX";
+        String romanNotation = "MMMXC";
         NotationConverter notationConverter = new NotationConverter(romanNotation);
         System.out.println(notationConverter.toArabic(romanNotation));
     }
--- src/NotationConverter.java
+++ src/NotationConverter.java
@@ -1,39 +1,30 @@
-
-
-
 public class NotationConverter {
-    private final char[] romanKeysArray = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-    private final int[] arabicKeysArray = {1, 5, 10, 50, 100, 500, 1000};
-    String romanNotation;
+    public int toArabic(String romanNotation) throws InvalidValueException{
+        checkNullInputString(romanNotation);
 
-    public NotationConverter(String romanNotation) throws InvalidValueException {
-        this.romanNotation = romanNotation;
-    }
+        inputCharArray = romanNotation.toCharArray();
 
-    public int toArabic(String romanNotation) {
-        char[] inputCharArray = InputConvertor(romanNotation);
         int[] inputArabicArray = new int[inputCharArray.length];
 
-            for (int i = 0; i < inputCharArray.length; i++) {
-                for (int j = 0; j < romanKeysArray.length; j++) {
-                    //try{
-                    if (inputCharArray[i] == romanKeysArray[j]) {
-                        inputArabicArray[i] = arabicKeysArray[j];
-                        //}else {throw new };
-                    }
-               // }catch (inputArabicArray[i] != arabicKeysArray[j]) new InvalidValueException("Incorrect symbol");
-            }
-        }
+        ConvertCharToIntArray(inputCharArray, inputArabicArray);
+
+        checkIncorrectRomanSymbols(inputArabicArray);
+
         return ResultCalculator(inputArabicArray);
     }
 
-    private char[] InputConvertor(String inputRomanNumber) {
-        char[] inputRomanCharArray = inputRomanNumber.toCharArray();
-        return inputRomanCharArray;
+    private final char[] romanKeysArray = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+    private final int[] arabicKeysArray = {1, 5, 10, 50, 100, 500, 1000};
+    char[] inputCharArray;
+    String errorMessage = "String must contain only valid roman numerals [I, V, X, L, C, D, M]";
+    String romanNotation;
+
+    public NotationConverter(String romanNotation) {
+        this.romanNotation = romanNotation;
     }
 
     public int ResultCalculator(int[] inputArray) {
-        int result = 0;
+        int result;
         if (inputArray.length == 1) return inputArray[0];
 
         if (inputArray.length == 2) {
@@ -48,7 +39,7 @@ public class NotationConverter {
 
 
     private int TwoLengthNumberCalculator(int[] inputArray) {
-        int result = 0;
+        int result;
         System.out.println("TwoLength method running..");
         if (inputArray[0] < inputArray[1]) {
             result = (inputArray[1] - inputArray[0]);
@@ -61,13 +52,13 @@ public class NotationConverter {
     }
 
     private int ThreeAndMoreNumberCalculator(int[] inputArray) {
-        int result = 0;
+        int result;
         if (inputArray[0] < inputArray[1]) {
             result = -inputArray[0] + inputArray[inputArray.length - 1];
         } else {
             result = inputArray[0] + inputArray[inputArray.length - 1];
         }
-        ;
+
         for (int i = 1; i < inputArray.length - 1; i++) {
             if (inputArray[i] >= inputArray[i + 1]) {
                 result += inputArray[i];
@@ -77,5 +68,33 @@ public class NotationConverter {
 
         }
         return result;
+    }
+
+    private void checkNullInputString(String inputString) {
+        if (inputString.equals("")) throw new InvalidValueException(errorMessage);
+    }
+
+    private void checkIncorrectRomanSymbols(int[] inputArabicArray) {
+        for (int i = 0; i < inputArabicArray.length; i++) {
+            if (inputArabicArray[i] == 0) {
+                throw new InvalidValueException(errorMessage);
+            }
+
+
+        }
+    }
+
+    private void ConvertCharToIntArray(char[] inputCharArray, int[] inputArabicArray) {
+
+        for (int i = 0; i < inputCharArray.length; i++) {
+            for (int j = 0; j < romanKeysArray.length; j++) {
+                if (inputCharArray[i] == romanKeysArray[j]) {
+                    inputArabicArray[i] = arabicKeysArray[j];
+
+                }
+
+            }
+        }
+
     }
 }
